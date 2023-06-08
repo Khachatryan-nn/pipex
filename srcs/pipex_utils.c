8f2d4baceb40f8e8135f2bd2f00a3832008cf150
Khachatryan-nn <tigrankh02@gmail.com>
2023-06-08 10:27:55 +0400

    fixed norminatte errors

--- srcs/pipex_utils.c
+++ srcs/pipex_utils.c
@@ -6,7 +6,7 @@
 /*   By: tikhacha <tikhacha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 15:51:30 by tikhacha          #+#    #+#             */
-/*   Updated: 2023/06/08 09:25:18 by tikhacha         ###   ########.fr       */
+/*   Updated: 2023/06/08 10:26:55 by tikhacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,13 +38,11 @@ void	child_start(t_pipex *pipex, int i)
 		close(pipex->pipes[j][0]);
 		close(pipex->pipes[j][1]);
 	}
-	if (execve(pipex->cmd_path, pipex->cmd_args, pipex->path) == -1)
+	if (execve(pipex->cmd_path, pipex->cmd_args, pipex->path) == -1 || \
+		execve(pipex->cmd_args[0], pipex->cmd_args, pipex->path) == -1)
 	{
-		if (execve(pipex->cmd_args[0], pipex->cmd_args, pipex->path) == -1)
-		{
-			perror("execve_Err");
-			exit (1);
-		}
+		perror("execve_Err");
+		exit (1);
 	}
 }
 
@@ -69,13 +67,11 @@ void	child_middle(t_pipex *pipex, int i)
 		close(pipex->pipes[j][0]);
 		close(pipex->pipes[j][1]);
 	}
-	if (execve(pipex->cmd_path, pipex->cmd_args, pipex->path) == -1)
+	if (execve(pipex->cmd_path, pipex->cmd_args, pipex->path) == -1 || \
+		execve(pipex->cmd_args[0], pipex->cmd_args, pipex->path) == -1)
 	{
-		if (execve(pipex->cmd_args[0], pipex->cmd_args, pipex->path) == -1)
-		{
-			perror("execve_Err");
-			exit (1);
-		}
+		perror("execve_Err");
+		exit (1);
 	}
 }
 
@@ -100,13 +96,11 @@ void	child_end(t_pipex *pipex, int i)
 		close(pipex->pipes[j][0]);
 		close(pipex->pipes[j][1]);
 	}
-	if (execve(pipex->cmd_path, pipex->cmd_args, pipex->path) == -1)
+	if (execve(pipex->cmd_path, pipex->cmd_args, pipex->path) == -1 || \
+		execve(pipex->cmd_args[0], pipex->cmd_args, pipex->path) == -1)
 	{
-		if (execve(pipex->cmd_args[0], pipex->cmd_args, pipex->path) == -1)
-		{
-			perror("execve_Err");
-			exit (1);
-		}
+		perror("execve_Err");
+		exit (1);
 	}
 }
 
